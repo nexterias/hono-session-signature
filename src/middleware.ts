9f3e58609987c3d47a3fb4ec9562c6f87168c37b
InkoHX <me@inkohx.dev>
2023-06-29 10:26:07 +0900

    fix: replace HTTPException throwing with JSON response

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,17 +17,17 @@ export const cookieSignature = (
   return async (context, next) => {
     for (const cookieKey of options.cookies) {
       const rawCookieValue = getCookie(context, cookieKey)
-      if (!rawCookieValue) throw new HTTPException(400)
+      if (!rawCookieValue) return context.json({ message: `Cookie(${cookieKey}) is required.` }, 400)
 
       const splittedCookieValue = splitCookieValue(rawCookieValue)
       if (!splittedCookieValue.value || !splittedCookieValue.signature)
-        throw new HTTPException(400)
+        return context.json({ message: 'Invalid cookie value' }, 400)
 
       const signature = decodeBase64Url(splittedCookieValue.signature)
       const cookieValue = decodeBase64Url(splittedCookieValue.value)
 
       const verified = await verify(options.secret, signature, cookieValue)
-      if (!verified) throw new HTTPException(401)
+      if (!verified) return context.json({ message: 'Invalid signature' }, 400)
 
       const decoder = new TextDecoder()
       context.set(getContextKey(cookieKey), decoder.decode(cookieValue))
