30c8c50790cb8b75d137a2f5950d27b6162c6812
InkoHX <me@inkohx.dev>
2023-06-30 00:27:00 +0900

    feat: add `getCookie` function

--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,10 @@
 import { Context as HonoContext } from 'hono'
-import { setCookie } from 'hono/cookie'
+import { setCookie, getCookie as _getCookie } from 'hono/cookie'
 import type { CookieOptions } from 'hono/utils/cookie'
-import { sign } from './signature'
+import { sign, verify } from './signature'
 import { encodeBase64Url } from './utils/base64url'
 import { getContextKey } from './utils/context'
-import { concatCookieValue } from './utils/cookie'
+import { concatCookieValue, splitCookieValue } from './utils/cookie'
 
 /**
  * Function to set a signed cookie in the Set-Cookie header
@@ -48,10 +48,44 @@ export const setCookieWithSignature =
  * @param context - The Hono context object.
  * @param name - The name of the cookie to retrieve.
  * @returns The verified cookie value, or undefined if the cookie is not found or is invalid.
+ *
+ * **WARN: Use this function only when using the `cookieSignature` middleware.**
  */
 export const getVerifiedCookie = (context: HonoContext, name: string) => {
   const value = context.get(getContextKey(name))
 
   if (typeof value !== 'string') return
   else return value
+}
+
+/**
+ * Retrieves and verifies a cookie value
+ *
+ * @param context - The Hono context object.
+ * @param key - The key used for signing the cookie.
+ * @param name - The name of the cookie to retrieve.
+ * @returns The verified cookie value, or undefined if the cookie is not found or is invalid.
+ *
+ * **If you are using the `cookieSignature` middleware, it is recommended to use `getVerifiedCookie`.**
+ */
+export const getCookie = async (
+  context: HonoContext,
+  key: CryptoKey,
+  name: string
+) => {
+  const rawCookieValue = _getCookie(context, name)
+  if (!rawCookieValue) return
+
+  const { signature, value } = splitCookieValue(rawCookieValue)
+  if (!signature || !value) return
+
+  const encoder = new TextEncoder()
+  const verified = await verify(
+    key,
+    encoder.encode(signature),
+    encoder.encode(value)
+  )
+  if (!verified) return
+
+  return value
 }
